750a333695fd9797b4eaa77d0effe2b82b62223d
Nikhiladiga <nikhiladigaz@gmail.com>
2026-07-23 21:26:35 +0530

    chore: load SECRET_KEY from env and add requirements.txt
    - SECRET_KEY loaded via os.getenv with insecure fallback for dev
    - Add DJANGO_SECRET_KEY to .env.example
    - Add requirements.txt with pinned dependency ranges

--- typesense-django-full-text-search/typesensedjango/settings.py
+++ typesense-django-full-text-search/typesensedjango/settings.py
@@ -25,7 +25,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*x$rqyx0w#d1kz*=t@vhn87-%f#bmml=9sc4a8i2pbo6q+!!&f'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-*x$rqyx0w#d1kz*=t@vhn87-%f#bmml=9sc4a8i2pbo6q+!!&f')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
